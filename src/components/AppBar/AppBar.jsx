87f4c6fae6ff1b99e17d2a78b5318d1a09072a39
Andrii <a.qwerty0444@gmail.com>
2025-01-16 23:23:53 +0200

    add new styles

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { apiLogoutUser } from "../../redux/auth/operations";
 import { selectUserData } from "../../redux/auth/selectors";
-
+import css from "./AppBar.module.css";
 const AppBar = () => {
   const dispatch = useDispatch();
   const onLogOut = () => {
@@ -9,10 +9,12 @@ const AppBar = () => {
   };
   const userData = useSelector(selectUserData);
   return (
-    <div>
-      <div>{userData.name[0]}</div>
-      <p>{userData.name}</p>
-      <button type="button" onClick={onLogOut}>
+    <div className={css.UserBarWrapper}>
+      <div className={css.UserBarUser}>
+        <div className={css.UserBarAvatar}>{userData.name[0]}</div>
+        <p className={css.UserBarName}>{userData.name}</p>
+      </div>
+      <button className={css.UserBarBtn} type="button" onClick={onLogOut}>
         Log out
       </button>
     </div>
